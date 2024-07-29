464ac2aac8c5d544449f35cfef235883bfc104cd
samson <samson.nigusse7@gmail.com>
2024-07-29 11:01:17 -0500

    new products added

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="#">store@HindiStore.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>10am to 7pm </p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://maps.app.goo.gl/F3YEZU7YDRr3f1NX9">208 W Main St, Marshall, MN</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+15079293881">507-929-3881</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Hini Store Marshall</span> | all rights reserved!</div>
+
+</footer>
+
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
